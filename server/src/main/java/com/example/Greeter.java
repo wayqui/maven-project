b323b3e6f6e4b4b440a1d010ad8b52416ee62196
wayqui <joselobustamante@gmail.com>
2018-08-20 18:46:51 +0200

    Add files via upload

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,15 +5,19 @@ package com.example;
  */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
+	/**
+	 * This is a constructor.
+	 */
+	public Greeter() {
 
-  }
+	}
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+	/**
+	 * Clase que define el saludo
+	 * @param someone
+	 * @return resultado
+	 */
+	public final String greet(final String someone) {
+		return String.format("Hello, %s!", someone);
+	}
 }
